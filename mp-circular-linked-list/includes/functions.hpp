c9245a18bbf064044d33cf91bf5c2531892d2012
Zhilan Wang <92745699+niconicoo0w0@users.noreply.github.com>
2023-01-31 00:02:13 -0600

    Delete mp-circular-linked-list directory

--- mp-circular-linked-list/includes/functions.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_HPP
-#define FUNCTIONS_HPP
-
-#include "utilities.hpp"
-
-int FIND(const std::string& dna, const std::string& subdna);
-std::string RESULT(const std::string& dnaline, std::vector<std::string> filebyline);
-
-#endif
